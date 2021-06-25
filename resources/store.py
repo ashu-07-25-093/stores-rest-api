9af9813625b70a5935ca6d309263e73ab2277a02
ashu25093 <ashu25093@gmail.com>
2021-06-25 17:42:04 +0530

    First commit, added REST API to access items,stores and user authentication

---
+++ resources/store.py
@@ -0,0 +1,39 @@
+from flask_restful import Resource
+from Section6.Code.models.store import StoreModel
+
+class Store(Resource):
+
+    def get(self,name):
+
+        store = StoreModel.find_by_name(name)
+        if store:
+            return store.json()
+        else:
+            return {"message" : "Store not found"}, 404
+
+    def post(self,name):
+
+        if StoreModel.find_by_name(name):
+            return {"message" : "A store with name '{}' already exists".format(name)}, 400
+
+        store = StoreModel(name)
+        try:
+            store.save_to_db()
+        except:
+            return {"message" : "An error occurred while creating store"}, 500
+        return store.json(), 201
+
+    def delete(self,name):
+
+        store = StoreModel.find_by_name(name)
+        if store:
+            store.delete_from_db()
+            return {"message" : "Store deleted Successfully"}, 201
+        else:
+            return {"message" : "Store not found"}, 404
+
+
+class StoreList(Resource):
+
+    def get(self):
+        return {"stores" : [store.json() for store in StoreModel.query.all()]}
---
+++ resources/user.py
@@ -0,0 +1,20 @@
+from flask_restful import Resource, reqparse
+from Section6.Code.models.user import UserModel
+
+class UserRegister(Resource):
+
+    parser = reqparse.RequestParser()
+    parser.add_argument('username',type=str,required=True,help="This field can not be left blank")
+    parser.add_argument('password',type=str,required=True,help="This field can not be left blank")
+
+    def post(self):
+        print('in post')
+        data = UserRegister.parser.parse_args()
+
+        if UserModel.find_by_username(data['username']):
+            return {"message" : "A user with this username is already exists"}, 400
+
+        user = UserModel(**data)    # UserModel(data['username'], data['password'])
+        user.save_to_db()
+
+        return {"message" : "User created Successfully"}, 201
